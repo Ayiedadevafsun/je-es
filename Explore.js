fc3700e46b61d0638ac6b4678ff4d30d74a512fc
Ayiedadevafsun <96953293+Ayiedadevafsun@users.noreply.github.com>
2026-06-13 10:41:05 +0700

    Update Explore.js

--- Explore.js
+++ Explore.js
@@ -1,105 +1,52 @@
-function openTab(id,btn){
+function openTab(id, btn){
 
   document.querySelectorAll('.tab-content')
-  .forEach(el=>el.classList.remove('active'));
+    .forEach(el => el.classList.remove('active'));
 
   document.querySelectorAll('.tab-btn')
-  .forEach(el=>el.classList.remove('active'));
+    .forEach(el => el.classList.remove('active'));
 
-  document.getElementById(id)
-  .classList.add('active');
-
+  document.getElementById(id).classList.add('active');
   btn.classList.add('active');
 }
 
-function createLinks(target,data,type,param,label){
+function createLinks(target, data, type, param, suffix){
 
-  let html='';
+  let html = '';
 
-  data.forEach(item=>{
-
+  data.forEach(item => {
     html += `
       <a href="/p/explore-movies-tv-shows.html?type=${type}&${param}=${item[0]}">
-        ${item[1]} ${label}
+        ${item[1]} ${suffix}
       </a>
     `;
-
   });
 
   document.getElementById(target).innerHTML = html;
 }
 
-fetch('https://raw.githubusercontent.com/Ayiedadevafsun/je-es/main/X.json')
-.then(r=>r.json())
-.then(data=>{
+fetch('https://raw.githubusercontent.com/Ayiedadevafsun/je-es/refs/heads/main/X.json')
+  .then(res => res.json())
+  .then(data => {
 
-  createLinks(
-    'movieGenres',
-    data.movieGenres,
-    'movie',
-    'genre',
-    'Movies'
-  );
+    createLinks('movieGenres', data.movieGenres, 'movie', 'genre', 'Movies');
+    createLinks('tvGenres', data.tvGenres, 'tv', 'genre', 'TV Shows');
 
-  createLinks(
-    'tvGenres',
-    data.tvGenres,
-    'tv',
-    'genre',
-    'TV Shows'
-  );
+    createLinks('movieCountries', data.countries, 'movie', 'country', 'Movies');
+    createLinks('tvCountries', data.countries, 'tv', 'country', 'TV Shows');
 
-  createLinks(
-    'movieCountries',
-    data.countries,
-    'movie',
-    'country',
-    'Movies'
-  );
+    createLinks('movieLanguages', data.languages, 'movie', 'language', 'Movies');
+    createLinks('tvLanguages', data.languages, 'tv', 'language', 'TV Shows');
 
-  createLinks(
-    'tvCountries',
-    data.countries,
-    'tv',
-    'country',
-    'TV Shows'
-  );
+    let movieYears = '';
+    let tvYears = '';
 
-  createLinks(
-    'movieLanguages',
-    data.languages,
-    'movie',
-    'language',
-    'Movies'
-  );
+    for(let year = new Date().getFullYear(); year >= 1950; year--){
+      movieYears += `<a href="/p/explore-movies-tv-shows.html?type=movie&year=${year}">Movies ${year}</a>`;
+      tvYears += `<a href="/p/explore-movies-tv-shows.html?type=tv&year=${year}">TV Shows ${year}</a>`;
+    }
 
-  createLinks(
-    'tvLanguages',
-    data.languages,
-    'tv',
-    'language',
-    'TV Shows'
-  );
+    document.getElementById('movieYears').innerHTML = movieYears;
+    document.getElementById('tvYears').innerHTML = tvYears;
 
-});
-
-let movieYears='';
-let tvYears='';
-
-for(let year=new Date().getFullYear();year>=data.startYear;year--){
-
-  movieYears += `
-    <a href="/p/explore-movies-tv-shows.html?type=movie&year=${year}">
-      Movies ${year}
-    </a>
-  `;
-
-  tvYears += `
-    <a href="/p/explore-movies-tv-shows.html?type=tv&year=${year}">
-      TV Shows ${year}
-    </a>
-  `;
-}
-
-document.getElementById('movieYears').innerHTML = movieYears;
-document.getElementById('tvYears').innerHTML = tvYears;
+  });
